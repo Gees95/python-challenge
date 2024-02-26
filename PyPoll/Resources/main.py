817ff7c39384c93f21f607aa17b31823d7b70120
Gees95 <reachdaoud@gmail.com>
2024-02-26 23:23:29 +0000

    added text file

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -1,17 +1,27 @@
 import csv
+import os
+
+
+#path to txt file
+text_file_path = "Election_result.txt"
 
 # Path to the CSV file
 filename = 'election_data.csv'
+
+
 
 # Initialize variables
 total_votes = 0
 candidates = {}
 winner = ''
 max_votes = 0
+winner_candidate = []
 
 # Open the CSV file and process the data
 with open(filename, mode='r') as csvfile:
     csvreader = csv.DictReader(csvfile)
+
+    #ignoring the titles 
     
     for row in csvreader:
         total_votes += 1
@@ -29,9 +39,14 @@ print(f"Total Votes: {total_votes}")
 print("-------------------------")
 
 for candidate, votes in candidates.items():
-    percentage = (votes / total_votes) * 100
-    print(f"{candidate}: {percentage:.3f}% ({votes})")
     
+    percentage = (votes / total_votes) * 100
+        
+    print(f"{candidate}: {percentage:.3f}% ({votes})")
+    winner_candidate.append(f"{candidate}: {percentage:.3f}% ({votes})")
+
+
+
     if votes > max_votes:
         max_votes = votes
         winner = candidate
@@ -39,3 +54,23 @@ for candidate, votes in candidates.items():
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
+
+
+#txt file
+election_output = (
+    f'Election Results\n'
+    f'---------------------------------\n'
+    f'Total Votes: {total_votes}\n'
+    f'---------------------------------\n'
+    f'{winner_candidate}\n'
+    f'---------------------------------\n'
+    f'Winner: {winner}'
+)
+
+print(election_output)
+with open(text_file_path, 'w') as text_file:
+    text_file.write(election_output)
+    
+
+
+print('Election results have been saved to: ', text_file_path)
--- Pybank/Resources/main.py
+++ Pybank/Resources/main.py
@@ -1,7 +1,11 @@
 import csv
-
+import os
 #opening your file
 
+text_file_path = "Financial_Analysis.txt"
+
+
+csv_reader = ['Date', 'Profit/losses']
 
 # intiating your vairables 
 total_months = 0
@@ -18,8 +22,10 @@ greatest_decrease = ["", 99999999999999]
 with open('budget_data.csv', 'r') as file:
     csv_reader = csv.reader(file)
 
+
     #ignoring the titles 
-    next(csv_reader)
+    csv_header= next(csv_reader)
+
     for row in csv_reader:
         #adding up the number of months 
         total_months += 1
@@ -41,6 +47,7 @@ with open('budget_data.csv', 'r') as file:
                 greatest_decrease[0]= row[0]
                 greatest_decrease[1] = row[1]
                 greatest_decrease[1] = change
+                
 
         #now we updata the value of previous monthly profit/loss value
         previous_month_profit_losses = int(row[1])
@@ -48,13 +55,25 @@ with open('budget_data.csv', 'r') as file:
 average_change = sum(monthly_change) / len(monthly_change)
 
 
-print("financial Analysis")     
-print('------------------------------------')
-print(f'Total months:', total_months)
 
-print(f'Net total amount: $',net_Profit_Losses)
+other_output = (
+    f'Fanancial Analysis\n'
+    f'---------------------------------\n'
+    f'Total Months: {total_months}\n'
+    f'Total: ${net_Profit_Losses}\n'
+    f'Average Change: ${round(average_change), 2}\n'
+    f'Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n'
+    f'Greates Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n'
+)
 
-print(f'Average change is : ${round(average_change, 2)}')
+print(other_output)
 
-print(f"Greatest Increase in Profits {greatest_increase[0]} (${greatest_increase[1]})")
-print(f"Greatest Decrease in Profit: {greatest_decrease[0]} (${greatest_decrease[1]})")
+
+
+
+
+with open(text_file_path, 'w') as text_file:
+    text_file.write(other_output)
+
+print('Financial analysis has been saved to:', text_file_path)
+
